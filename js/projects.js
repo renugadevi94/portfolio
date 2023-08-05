9a21ddfe627d695aa7caaf00a43671bade7ea67f
renugadevi94 <srenugadevi1991@gmail.com>
2023-08-05 22:23:20 +0530

    portfolio

---
+++ js/projects.js
@@ -0,0 +1,43 @@
+const projects = [{
+    img: "./image/sur.webp",
+    name: "SurvayForm",
+    category: "frontend",
+    techstack: ['html5', 'css3', 'javascript'],
+    frontend: "https://github.com/renugadevi94/guvi-bootstrap/blob/master/survay.html",
+    deploymentUrl: "https://renugadevi94.github.io/guvi-bootstrap/survay.html",
+    description: `Responsive survay Form Application.`
+},
+{
+    img: "./image/country.png",
+    name: "Rest Countries",
+    category: "Advance Javascript",
+    techstack: ['html5', 'css3', 'javascript'],
+    frontend: "https://github.com/renugadevi94/weather",
+    deploymentUrl: "https://steady-buttercream-ea8e2b.netlify.app/",
+    description: `Rest Countries and weather using Fetch API.`
+},
+
+{
+    img: "./image/dict.jpeg",
+    name: "Dictionary",
+    category: "frontend",
+    techstack: ['html5', 'css', 'javascript'],
+    frontend: "https://github.com/renugadevi94/API1",
+    deploymentUrl: "https://jade-bunny-3caab8.netlify.app/",
+    description: `Create simple dictionary using mock api `
+},
+
+{
+    img: "./image/hall.jpeg",
+    name: "Book My Hall",
+    category: "backend",
+    techstack: ['html5', 'css-3', 'bootstrap-4', 'mongo', 'nodejs'],
+    frontend: "https://github.com/renugadevi94/hall-booking",
+    backend: "https://hall-booking-4ni4.onrender.com/rooms/all",
+    description: `A Hall booking system developed the backend with the help of Node.JS and Express.Js`
+}
+
+
+
+]
+
